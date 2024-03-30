fad482ea49e591659d03f53381041bf7ba1b5d95
Ferclemens <69605971+Ferclemens@users.noreply.github.com>
2024-03-30 16:23:32 -0300

    added cypress for testing and change margins from hero

--- components/intro.tsx
+++ components/intro.tsx
@@ -7,7 +7,7 @@ import { TypeAnimation } from "react-type-animation";
 export default function Intro() {
   return (
     <motion.section
-      className="flex items-center justify-center my-32 mx-7 flex-col-reverse sm:flex-row sm:mx-10 sm:my-[4.3rem] lg:my-20"
+      className="flex items-center justify-center mt-[10rem] mx-7 flex-col-reverse sm:flex-row sm:mx-10 sm:my-[4.3rem] lg:my-20"
       initial={{ y: -100, opacity: 0 }}
       animate={{ y: 0, opacity: 1 }}
     >
@@ -16,7 +16,7 @@ export default function Intro() {
         alt="personal image to presentation"
         width={300}
         height={300}
-        className="pt-10 sm:p-4 lg:p-0"
+        className="pt-24 sm:p-4 lg:p-0"
       />
       <div className="text-gray-600 flex flex-col sm:p-4 lg:p-16">
         <h1 className="text-4xl lg:text-6xl font-bold">
@@ -40,13 +40,14 @@ export default function Intro() {
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia fugiat
           ipsum!
         </p>
-        <div className="flex justify-between  items-center text-center">
-          <button className="p-3 w-22 sm:w-28 lg:w-40 bg-gradient-to-tr from-blue-500 to-teal-400 rounded-full font-bold text-xs sm:text-sm lg:text-lg text-slate-200">
+        <div className="flex justify-between  items-center text-center sm:max-w-[28rem] lg:max-w-[30rem]">
+          <button className="p-3 w-28 sm:w-36 lg:w-40 bg-gradient-to-tr from-teal-400 to-blue-500 rounded-full font-bold text-xs sm:text-sm lg:text-lg text-slate-200 hover:scale-110 transition">
             Contact Me!
           </button>
           <a
-            href=""
-            className="p-3 w-22 sm:w-28 lg:w-40 bg-gradient-to-tr from-blue-500 to-teal-400 rounded-full font-bold text-xs sm:text-sm lg:text-lg text-slate-200"
+            href="/ferclemens-cv.pdf"
+            download
+            className="p-3 w-28 sm:w-36 lg:w-40 bg-gradient-to-tr from-blue-500 to-teal-400 rounded-full font-bold text-xs sm:text-sm lg:text-lg text-slate-200 hover:scale-110 transition"
           >
             Dowload CV
           </a>
@@ -56,6 +57,7 @@ export default function Intro() {
               alt="LinkedIn logo"
               width={30}
               height={30}
+              className="hover:scale-110 transition"
             />
           </a>
           <a href="https://github.com/Ferclemens" target="_blank">
@@ -64,6 +66,7 @@ export default function Intro() {
               alt="GitHub logo"
               width={32}
               height={32}
+              className="hover:scale-125 transition"
             />
           </a>
         </div>
